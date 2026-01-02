c1763ff2a768ae4d0a13c567355fe372c2bd6621
shashwat-sahuu <140780125+shashwat-sahuu@users.noreply.github.com>
2026-01-03 00:20:02 +0530

    Update run_pipeline.sh

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+
+echo "Starting Variant Calling Pipeline..."
+
+FASTQ1=$1
+FASTQ2=$2
+REF=$3
+
+bwa index $REF
+bwa mem $REF $FASTQ1 $FASTQ2 > aligned.sam
+
+samtools view -Sb aligned.sam > aligned.bam
+samtools sort aligned.bam -o sorted.bam
+samtools index sorted.bam
+
+bcftools mpileup -f $REF sorted.bam | \
+bcftools call -mv -o variants.vcf
+
+echo "Pipeline Completed Successfully"
 
